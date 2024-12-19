c3d7d1a011f79ecbb668d6b10a93b86e496b4318
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-12-19 12:55:30 +0530

    printing without using get method with using enhanced for loop in arraylist

---
+++ CWS_01_ArrayList/CWS_print_without_using_get_method.java
@@ -0,0 +1,15 @@
+package CWS_01_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CWS_print_without_using_get_method {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4));
+
+        /// using enhanced for loop
+        for (int i : list){
+            System.out.println(i);
+        }
+    }
+}
