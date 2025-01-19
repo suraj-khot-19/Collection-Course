fd00ac13cd22f69c1ae0e6e7a6ed20fe9a666bf2
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-19 16:25:42 +0530

    creating array list way1

---
+++ CWS_01_ArrayList/CWS_01_Create_ArrayList_1.java
@@ -0,0 +1,9 @@
+package CWS_01_ArrayList;
+
+import java.util.ArrayList;
+
+public class CWS_01_Create_ArrayList_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+    }
+}
