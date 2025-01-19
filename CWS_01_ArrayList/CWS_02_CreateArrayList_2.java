ac643828e2649c23627c23a763fc06cc8930b67a
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-19 16:29:39 +0530

    creating array list way2

---
+++ CWS_01_ArrayList/CWS_02_CreateArrayList_2.java
@@ -0,0 +1,18 @@
+package CWS_01_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CWS_02_CreateArrayList_2 {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+
+        /// because arrayList is implementing List interface
+
+        /*
+        Collections(interface) -
+               extends ->      |_ List(interface) -
+               implements ->                      |_ ArrayList (class)
+         */
+    }
+}
