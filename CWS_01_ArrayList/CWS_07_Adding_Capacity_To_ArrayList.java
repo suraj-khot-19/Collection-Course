03dc5731c42502b29a90f1cd53a463565e80d9d7
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-19 16:53:28 +0530

    adding capacity to ArrayList

---
+++ CWS_01_ArrayList/CWS_07_Adding_Capacity_To_ArrayList.java
@@ -0,0 +1,11 @@
+package CWS_01_ArrayList;
+
+import java.util.ArrayList;
+
+public class CWS_07_Adding_Capacity_To_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>(20);  /// it is by default 10
+
+        System.out.println(list);
+    }
+}
