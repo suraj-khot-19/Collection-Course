21cb0dc804f0d5c6587924d5d4495441b5824a50
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-19 16:57:42 +0530

    add method in ArrayList

---
+++ CWS_01_ArrayList/CWS_08_add_method.java
@@ -0,0 +1,16 @@
+package CWS_01_ArrayList;
+
+import java.util.ArrayList;
+
+public class CWS_08_add_method {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList=new ArrayList<>();
+
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        arrayList.add(40);
+
+        System.out.println(arrayList);
+    }
+}
