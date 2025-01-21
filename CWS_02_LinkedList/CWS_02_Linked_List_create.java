51f1841aa4caf2d5028343bc830b269bb31d408d
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-21 22:01:50 +0530

    linked list creation in java

---
+++ CWS_02_LinkedList/CWS_02_Linked_List_create.java
@@ -0,0 +1,11 @@
+package CWS_02_LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CWS_02_Linked_List_create {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1=new LinkedList<>(); /// as it is part of util package
+        List<Integer> linkedList2=new LinkedList<>();   /// as it implements a List interface
+    }
+}
