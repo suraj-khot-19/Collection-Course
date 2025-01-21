059863ae20281bfbd4678978c13f6c0870dc66c2
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-21 22:04:17 +0530

    linked list add method in java

---
+++ CWS_02_LinkedList/CWS_03_LinkedList_add_method.java
@@ -0,0 +1,17 @@
+package CWS_02_LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CWS_03_LinkedList_add_method {
+    public static void main(String[] args) {
+        List<Integer> linkedList=new LinkedList<>();
+
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(8);
+
+        System.out.println(linkedList);
+    }
+}
