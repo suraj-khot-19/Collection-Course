72bc19a1d61d878d156c88309d191811e7079d77
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-22 12:55:30 +0530

    linked list creation

---
+++ CWS_02_LinkedList/CWS_04_LinkedList_Creation_1.java
@@ -0,0 +1,11 @@
+package CWS_02_LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CWS_04_LinkedList_Creation_1 {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        System.out.println(list);
+    }
+}
