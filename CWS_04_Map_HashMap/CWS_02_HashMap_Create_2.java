fc048dadd88c813f27431788abf05d58928dc5e3
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-27 12:55:30 +0530

    hashmap with initial capacity

---
+++ CWS_04_Map_HashMap/CWS_02_HashMap_Create_2.java
@@ -0,0 +1,10 @@
+package CWS_04_Map_HashMap;
+
+import java.util.HashMap;
+
+public class CWS_02_HashMap_Create_2 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>(20); /// initial capacity
+
+    }
+}
