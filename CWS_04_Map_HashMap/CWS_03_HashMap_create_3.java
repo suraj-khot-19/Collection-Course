377c1902d00196dd08ca4febc54b3a4ba3bda4de
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-28 12:55:30 +0530

    hashmap with initial capacity and load factor

---
+++ CWS_04_Map_HashMap/CWS_03_HashMap_create_3.java
@@ -0,0 +1,13 @@
+package CWS_04_Map_HashMap;
+
+import java.util.HashMap;
+
+public class CWS_03_HashMap_create_3 {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map=new HashMap<>(20,0.80f);
+
+        /// defaults
+        /// load factor => 0.75
+        /// initial capacity => 16
+    }
+}
